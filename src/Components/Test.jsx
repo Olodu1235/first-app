b5e01f8b4df632d1e082a4e33f83ad0243723fd6
rnwonder <60509154+rnwonder@users.noreply.github.com>
2023-09-20 09:01:27 +0100

    Update Test.jsx

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -3,9 +3,12 @@ import { cn } from "../utils/class";
 export default function Test() {
   return (
     <div
-      className={cn(`
+      className={cn(
+        `
       bg-carrotOrange font-ubuntu text-white
-    `)}
+    `,
+        newLandingPaddingHorizontal
+      )}
     >
       <h1>Just testing tailwind</h1>
     </div>
